599e6c11d0c539c2140b7300283ed787c72096c3
labibks <h.kawsarsheikh@gmail.com>
2025-08-23 12:51:48 +0600

    add get bonus section

--- home.js
+++ home.js
@@ -51,17 +51,84 @@ document.getElementById("withdraw-btn")
     document.getElementById('available-balance').innerText = totalNewBalance
 })
 
+// transfer mony feature
+
+document
+  .getElementById("transfer-button")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    const userAccountNumber = document.getElementById("user-number").value
+    const transferMonyPin = parseInt(document.getElementById("transfer-pin").value)
+    const amount = parseInt(document.getElementById("transfer-amount").value);
+
+    const availableBalance = parseInt(
+    document.getElementById("available-balance").innerText);
+    console.log(amount, availableBalance);
+
+    if(userAccountNumber.length < 11){
+        alert('incorrect account number')
+        return;
+    }
+
+    if(validPin !== transferMonyPin){
+        alert('invalid pin')
+        return;
+    }
+
+    const totalNewBalance = availableBalance - amount;
+    console.log(totalNewBalance);
+
+    document.getElementById("available-balance").innerText = totalNewBalance;
+  });
+
+//   get bonus feature
+
+document.getElementById("bonus-button")
+.addEventListener('click',function(e){
+    e.preventDefault()
+    
+})
+
 // toggoling method
 
+// add mony
 document.getElementById("add-button")
   .addEventListener("click", function () {
-  document.getElementById("cash-out-parent").style.display = "none";
-  document.getElementById("add-mony-parent").style.display = "block";
+    document.getElementById("cash-out-parent").style.display = "none";
+    document.getElementById("transfer-mony-parent").style.display = "none";
+    document.getElementById("get-bonus-parent").style.display = "none";
+    document.getElementById("add-mony-parent").style.display = "block";
 });
-
+// cash out
 document.getElementById("cash-out-btn")
 .addEventListener('click',function(){
     document.getElementById("add-mony-parent").style.display = "none";
+    document.getElementById("transfer-mony-parent").style.display = "none";
+    document.getElementById("get-bonus-parent").style.display = "none";
     document.getElementById("cash-out-parent").style.display = "block"
     
 })
+
+// transfer mony
+
+document.getElementById("transfer-mony-btn")
+.addEventListener('click',function(){
+    document.getElementById("add-mony-parent").style.display = "none";
+    document.getElementById("cash-out-parent").style.display = "none";
+    document.getElementById("get-bonus-parent").style.display = "none";
+    document.getElementById("transfer-mony-parent").style.display = "block";
+
+})
+
+// get bonus 
+
+document.getElementById("get-bonus-btn")
+.addEventListener('click',function(){
+    document.getElementById("add-mony-parent").style.display = "none";
+    document.getElementById("cash-out-parent").style.display = "none";
+    document.getElementById("transfer-mony-parent").style.display = "none";
+    document.getElementById("get-bonus-parent").style.display = "block";
+
+
+})
+
